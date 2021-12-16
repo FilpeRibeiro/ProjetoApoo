89b1349398685b3d8aa16a17e742c19a0b04cd18
FilpeRibeiro <filipe-negreti@hotmail.com>
2021-12-16 01:43:41 -0300

    video add

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let button = $('#botao')
+$('.video').addClass('none')
 button.on('click', function(){
-$.ajax({
-    url:`https://api.nasa.gov/planetary/apod?api_key=c0rZBhWA17eRlEVJekMQW8fFD2hAkMp4U9HaRH74&date=${date.value}`,
+$.ajax({url:`https://api.nasa.gov/planetary/apod?api_key=c0rZBhWA17eRlEVJekMQW8fFD2hAkMp4U9HaRH74&date=${date.value}`,
     success: function(result){
         $('#texto').html(`${result.explanation}`)
         $('#titulo').html(`Autor:${result.copyright}`)
-        $('#imagen').attr('src', (result.url))
-        console.log(result)
+        
+        if(result.media_type =='image'){
+            $('#imagen').attr('src', (result.url))
+            $('.video').addClass('none')
+            $('#imagen').removeClass('none')
+        }else{
+            $('.video').attr( 'src',(result.url))
+            $('.video').removeClass('none')
+            $('#imagen').addClass('none')
+        }
+
+       
     }
     })
 });
